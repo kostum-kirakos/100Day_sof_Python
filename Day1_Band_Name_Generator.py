77d38134aced70e4abbc3e9047aa23bb5b6f8b06
LKOUSTIM MOHAMED <132625229+kostum-kirakos@users.noreply.github.com>
2023-09-01 16:56:08 +0100

    1st programme

---
+++ Day1_Band_Name_Generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator.")
+Favorite_Animal = input("What is your favorite animale ?\n")
+Home_Town = input("what is the city you born in ? \n")
+print("your band name could be :" + Favorite_Animal + " " + Home_Town)
