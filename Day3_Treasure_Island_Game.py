196433e5e70c847d3819e854aa13b89bed505e64
LKOUSTIM MOHAMED <132625229+kostum-kirakos@users.noreply.github.com>
2023-09-03 19:05:45 +0100

    3rd  programme updated

--- Day3_Treasure_Island_Game.py
+++ Day3_Treasure_Island_Game.py
@@ -21,34 +21,57 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 *******************************************************************************
 ''')
 print("Welcome to Treasure Island.")
-print("Your mission is to find the treasure.") 
+print("Your mission is to find the treasure.")
 
-Q1 = input("you found a split way in the island do you want to go left or right?")
-q1 = Q1.lower()
-if q1 == 'left' :
-  print("Game Over")
-  exit
-elif q1 == 'right' :
-  Q2 = input("you meet an Aged witch she gives you to choose between two candy one red and the other is blue, which one you want to take?")
-  q2 = Q2.lower()
-  if q2 == 'blue':
-    print("Game Over")
-    exit
-  elif q2 == 'red':
-    Q3 = input("after you toke the candy she gaves you a key to open one of tree doors, wooden, steel ,and gold which one you go in? ")
-    q3 = Q3.lower()
-    if q3 == 'wooden' :
-      print("Burned by fire")
-      print("Game Over")
-      exit
-    elif q3 == 'steel' :
-      print("Eaten by beasts")
-      print("Game Over")
-      exit
-    elif q3 == 'gold':
-      print("You found the treasure, You Win")
-      exit
-    
-  
+# Initial choice
+print("\nYou find a split way on the island. Do you want to go left or right?")
+choice1 = input("Type 'left' or 'right': ").lower()
+
+if choice1 == 'left':
+    print("\nYou've chosen the left path.")
+    print("You come across a deep ravine. You can't proceed this way.")
+    print("Game Over.")
+elif choice1 == 'right':
+    # Candy choice
+    print("\nYou meet an aged witch who offers you two candies, one red and the other blue.")
+    choice2 = input("Which candy do you want to take, 'red' or 'blue'? ").lower()
+
+    if choice2 == 'blue':
+        print("\nThe blue candy was poisonous.")
+        print("Game Over.")
+    elif choice2 == 'red':
+        # Door choice
+        print("\nAfter you take the red candy, the witch gives you a key to open one of three doors: wooden, steel, and gold.")
+        choice3 = input("Which door do you want to open, 'wooden', 'steel', or 'gold'? ").lower()
+
+        if choice3 == 'wooden':
+            print("\nThe wooden door leads to a room filled with fire. You're burned alive.")
+            print("Game Over.")
+        elif choice3 == 'steel':
+            print("\nThe steel door leads to a dark room with hungry beasts. They devour you.")
+            print("Game Over.")
+        elif choice3 == 'gold':
+            print("\nYou open the golden door and find the treasure chest.")
+            print("Congratulations! You've found the treasure and won the game!")
+        else:
+            print("\nInvalid choice. You're trapped forever.")
+            print("Game Over.")
+    else:
+        print("\nInvalid choice. The witch curses you.")
+        print("Game Over.")
+else:
+    print("\nInvalid choice. You're lost on the island forever.")
+    print("Game Over.")
+
+
+
+
+
+
+
+
+
+
+
 
 
